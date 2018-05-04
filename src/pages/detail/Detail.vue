4bcc99d96272b52414bc1219d285cf1d77d4d536
chenbj2333 <1749402844@qq.com>
2018-05-04 11:55:24 +0800

    详情header

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -5,16 +5,20 @@
       :bannerImg="bannerImg"
       :sightName="sightName">
     </detail-banner>
+    <detail-header></detail-header>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
+import DetailHeader from './components/Header'
 import axios from 'axios'
+
 export default {
   name: 'Detail',
   components: {
-    DetailBanner
+    DetailBanner,
+    DetailHeader
   },
   data () {
     return {
